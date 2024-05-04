79a6b6eb9ddae57748b74aed866919e2198f9656
javie <javie@DESKTOP-4OIRLS7>
2024-05-04 13:43:59 +0200

    Revert "Revert "Carga Actualizando""
    This reverts commit 4e24dc12550787c853838ec52208b65fc20bb168.

---
+++ src/main/java/Programa/InfoInmuebles.java
@@ -0,0 +1,14 @@
+package Programa;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author javie
+ */
+public class InfoInmuebles {
+    
+}
